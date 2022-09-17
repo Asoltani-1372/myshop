27ae15a4ac0d0e5a45da3eaccc7774c7faee8525
a.soltani <soltaniali.1994@gmail.com>
2022-09-17 22:38:36 +0430

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="container"> 
+            <Navbar />
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
